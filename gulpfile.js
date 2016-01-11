970a4278c3ef15bb13d87ee156cea2222fb25eb3
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-10 23:41:18 -0500

    Adding gulp tasks for browserify and watchify

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+
+var customOpts = {
+  entries: ['./js/main.js'],
+  debug: true
+};
+
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts)); 
+
+
+gulp.task('watch', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./js'));
+}
