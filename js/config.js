7a3c0b50427937663b1c28ec485a041d657d8ffd
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-13 09:36:42 -0500

    Adding basic configurations

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/**
+ * Module containing configurations for simulation.
+*/
+
+var CONFIG = {
+	height: 600,
+	width:  600, 
+	lineColor: 'black',
+	fillColor: 'green',
+	lineWidth: 0.5,
+	cellSize:  15
+}
+
+module.exports = {
+	config: CONFIG
+}
