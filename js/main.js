eb86716c49978160836d91a8809f4ec5e40864cf
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-25 22:49:44 -0500

    Removing function call

--- js/main.js
+++ js/main.js
@@ -12,6 +12,5 @@ const CONFIG = {
 const canvas = document.getElementById('grid');
 const context = canvas.getContext('2d');
 const draw = new Draw();
-draw.helloWorld();
 draw.renderGrid(canvas, context, CONFIG.cellSize, CONFIG.lineColor);
 draw.fillSquares(context, [{'x': 0, 'y': 0}, {'x': 7, 'y': 8}, {'x': 13, 'y': 5}]);
