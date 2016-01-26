708c69364d90e51db9341efe26c18a3eebef6dad
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-25 22:47:52 -0500

    Fixing webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-	entry: './js/main.js',
-	output: {
-		path: path.join(__dirname, 'js'),
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: path.join(__dirname, 'es6'),
-				loader: 'babel-loader'
-			}
-		]
-	}
+    entry: './js/main.js',
+    output: {
+        path: path.join(__dirname, 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'js'),
+                query: {
+                  presets: 'es2015',
+                },
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
 };
