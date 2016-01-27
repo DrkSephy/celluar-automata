a7a061c6f7db79137d0a1c05d1094fd2f7106d04
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-27 00:44:04 -0500

    Adding Simulation class

---
+++ js/simulation.js
@@ -0,0 +1,29 @@
+class Simulation {
+	constructor() {
+		this.simualationRunning = false;
+		this.simulationPaused = true;
+	}
+
+	runSimulation () {
+		this.simualationRunning = true;
+	}
+
+	pauseSimulation() {
+		this.simulationPaused = true;
+	}
+
+	resumeSimulation() {
+		this.simulationPaused = false;
+		this.simualationRunning = true;
+	}
+
+	togglePaused() {
+		if (this.simualationRunning) {
+			this.pauseSimulation();
+		} else {
+			this.resumeSimulation();
+		}
+	}
+}
+
+export default Simulation;
