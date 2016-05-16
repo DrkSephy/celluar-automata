d20ef258e23ac93ef669a75618cb2c6496cbd993
DrkSephy <dleonar00@citymail.cuny.edu>
2016-05-15 21:51:40 -0400

    Generalizing canvas layouts

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import Grid from './grid.js'
 
 const CONFIG = {
   height: 600,
-  width:  600, 
+  width:  1200, 
   lineColor: 'black',
   fillColor: 'green',
   lineWidth: 0.5,
@@ -13,7 +13,7 @@ const CONFIG = {
 const canvas = document.getElementById('grid');
 const context = canvas.getContext('2d');
 const draw = new Draw(canvas, context, CONFIG.cellSize, CONFIG.lineColor);
-const grid = new Grid(40, 40);
-grid.printGrid(40);
+const grid = new Grid(CONFIG.height / CONFIG.cellSize, CONFIG.width / CONFIG.cellSize);
+grid.printGrid(grid.rows);
 draw.renderGrid(canvas, CONFIG.cellSize, CONFIG.lineColor);
-draw.fillSquares([{'x': 0, 'y': 0}, {'x': 7, 'y': 8}, {'x': 13, 'y': 5}]);
+draw.fillSquares([{'x': 0, 'y': 0}, {'x': 7, 'y': 8}, {'x': 13, 'y': 5}, {'x': 25, 'y': 25}]);
